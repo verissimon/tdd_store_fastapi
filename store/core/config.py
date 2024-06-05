a392642d49f1d38e8e846b54eb3bbdffcab9120f
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2024-06-05 03:57:30 -0300

    configura mongodb client

--- store/core/config.py
+++ store/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME = "store fastapi"
+    PROJECT_NAME: str = "store fastapi"
     ROOT_PATH: str = "/"
     DATABASE_URL: str
     model_config = SettingsConfigDict(env_file=".env")
